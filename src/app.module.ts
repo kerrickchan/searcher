142fd821c2ec9047699cfeae476d974f6afbf8cb
Kerrick Chan <kerrickchan@icloud.com>
2024-03-05 23:08:11 +0800

    add postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { LlmModule } from './llm/llm.module';
-import { KafkaModule } from './kafka/kafka.module';
 import { ConfigModule } from '@nestjs/config';
+import { SwaggerModule } from '@nestjs/swagger';
+import { AppController } from './app.controller';
+import { DbModule } from './db/db.module';
+import { KafkaModule } from './kafka/kafka.module';
+import { LlmModule } from './llm/llm.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SwaggerModule,
+    DbModule,
     KafkaModule,
     LlmModule,
   ],
